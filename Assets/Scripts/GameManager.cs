3c8287b975633a0d1cca10ab3cd8c8a21e357e29
Vlad Rosu <86161545+vlandero@users.noreply.github.com>
2024-04-05 23:44:26 +0300

    solve platform camera (#12)

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,14 +5,18 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
-    public static float slowMotionTimeScale = 0.2f;
+    public static float slowMotionTimeScale = 0.4f;
+    public static float platformStopTime = 3f;
     public int score = 0;
     public GameObject ball;
     public GameObject platform;
-    public MoveArrow moveArrowComponent;
-    private Ball ballComponent;
-
-    private Rigidbody ballRb;
+    //public MoveArrow moveArrowComponent;
+    public Camera SideCamera;
+    public Camera PlatformCamera;
+    public Ball ballComponent;
+    public PlayerMovementPlatform playerMovementPlatform;
+    public LauchBall lauchBall;
+    public bool frozen = false;
 
     private void Awake()
     {
@@ -24,36 +28,35 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
-        ball = GameObject.FindGameObjectWithTag("ball");
-        ballRb = ball.GetComponent<Rigidbody>();
         ballComponent = ball.GetComponent<Ball>();
-        
-        platform = GameObject.FindGameObjectWithTag("platform");
-        moveArrowComponent = platform.GetComponent<MoveArrow>();
+        //moveArrowComponent = platform.GetComponent<MoveArrow>();
+        playerMovementPlatform = platform.GetComponent<PlayerMovementPlatform>();
+        lauchBall = platform.GetComponent<LauchBall>();
+        SeeSidePerspective();
     }
 
-    public Vector3 GetPlatformArrowDirection()
-    {
-        return moveArrowComponent.vectorToDisplay;
-    }
+    //public Vector3 GetPlatformArrowDirection()
+    //{
+    //    return moveArrowComponent.vectorToDisplay;
+    //}
 
     private void Update()
     {
         if (Mathf.Abs(ball.transform.position.y - platform.transform.position.y) < 3)
         {
-            if(ballComponent.IsMovingUp())
+            if (ballComponent.IsMovingUp())
             {
-                moveArrowComponent.DeactivateArrow();
+                //moveArrowComponent.DeactivateArrow();
                 Time.timeScale = 1;
             }
             else
             {
+                if (frozen) return;
                 Time.timeScale = slowMotionTimeScale;
-                moveArrowComponent.ActivateArrow();
+                //moveArrowComponent.ActivateArrow();
             }
-            
         }
-        if (ball.transform.position.y < platform.transform.position.y - 2)
+        if (ball.transform.position.y < platform.transform.position.y - 1)
         {
             GameOver();
         }
@@ -67,5 +70,21 @@ public class GameManager : MonoBehaviour
     public void AddScore(int value)
     {
         score += value;
+    }
+
+    public void SeePlatformPerspective()
+    {
+        PlatformCamera.gameObject.SetActive(true);
+        SideCamera.gameObject.SetActive(false);
+        playerMovementPlatform.enabled = false;
+        lauchBall.enabled = true;
+    }
+
+    public void SeeSidePerspective()
+    {
+        PlatformCamera.gameObject.SetActive(false);
+        SideCamera.gameObject.SetActive(true);
+        lauchBall.enabled = false;
+        playerMovementPlatform.enabled = true;
     }
 }
