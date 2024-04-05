3dd6fa0c9406f100b146944e205bfe88d79e4019
vlandero <vlad.andrei.rosu@gmail.com>
2024-04-05 21:53:40 +0300

    finalize arrow

--- Assets/Scripts/MoveArrow.cs
+++ Assets/Scripts/MoveArrow.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class MoveArrow : MonoBehaviour
 {
+    public Vector3 vectorToDisplay;
     public Color arrowColor = Color.red;
     public float rotationSpeed = 50f;
     public GameObject arrowInstance;
     public LayerMask wallMask;
+    public bool arrowVisible = true;
 
     private LineRenderer lineRenderer;
     private float rotationAngleX = 0f;
@@ -20,18 +22,23 @@ public class MoveArrow : MonoBehaviour
         lineRenderer.endColor = arrowColor;
         lineRenderer.startWidth = 0.1f;
         lineRenderer.endWidth = 0.1f;
+        DeactivateArrow();
     }
 
     void Update()
     {
+        if(!arrowVisible)
+        {
+            return;
+        }
         float rotationInputHor = Input.GetAxis("Horizontal");
         float rotationInputVer = Input.GetAxis("Vertical");
 
         if (rotationInputHor != 0 || rotationInputVer != 0)
         {
-            rotationAngleX += rotationSpeed * Time.deltaTime * rotationInputHor;
+            rotationAngleX += rotationSpeed * Time.deltaTime * rotationInputHor * (1 / GameManager.slowMotionTimeScale);
             rotationAngleX = Mathf.Clamp(rotationAngleX, -45, 45);
-            rotationAngleZ += rotationSpeed * Time.deltaTime * rotationInputVer;
+            rotationAngleZ += rotationSpeed * Time.deltaTime * rotationInputVer * (1 / GameManager.slowMotionTimeScale);
             rotationAngleZ = Mathf.Clamp(rotationAngleZ, -45, 45);
 
             arrowInstance.transform.rotation = Quaternion.Euler(rotationAngleX, 0, rotationAngleZ);
@@ -41,11 +48,24 @@ public class MoveArrow : MonoBehaviour
         {
             lineRenderer.SetPosition(0, transform.position);
             lineRenderer.SetPosition(1, hit.point);
+            vectorToDisplay = (hit.point - transform.position).normalized;
         }
         else
         {
             lineRenderer.SetPosition(0, Vector3.zero);
             lineRenderer.SetPosition(1, Vector3.zero);
         }
+    }
+
+    public void ActivateArrow()
+    {
+        arrowInstance.SetActive(true);
+        arrowVisible = true;
+    }
+
+    public void DeactivateArrow()
+    {
+        arrowInstance.SetActive(false);
+        arrowVisible = false;
     }
 }
