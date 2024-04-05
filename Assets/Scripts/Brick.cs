555d33446abd2d0424f65be00571f6365b5e8667
vlandero <vlad.andrei.rosu@gmail.com>
2024-04-05 15:51:57 +0300

    ball bounce

---
+++ Assets/Scripts/Brick.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick : MonoBehaviour
+{
+    public int health = 1;
+    public int score = 1;
+    
+    public void HandleHit()
+    {
+        health--;
+        if (health <= 0)
+        {
+            GameManager.instance.AddScore(score);
+            Destroy(gameObject);
+        }
+    }
+}
