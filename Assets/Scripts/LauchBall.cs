48bd08aa534216b1210e6f2c848e044d5dee9b2c
Vlad Rosu <86161545+vlandero@users.noreply.github.com>
2024-04-06 21:27:56 +0300

    solve smooth movement (#20)

--- Assets/Scripts/LauchBall.cs
+++ Assets/Scripts/LauchBall.cs
@@ -3,23 +3,28 @@ using UnityEngine;
 public class LauchBall : MonoBehaviour
 {
     public Vector3 launchDirection;
+    public Vector3 launchPosition;
     public LayerMask wallMask;
+    public GameObject hitMark;
 
     private void Start()
     {
         launchDirection = Vector3.up;
+        launchPosition = transform.position;
+        hitMark.SetActive(false);
     }
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        hitMark.SetActive(true);
+        Ray ray = GameManager.instance.PlatformCamera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, wallMask))
         {
-            Ray ray = GameManager.instance.PlatformCamera.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-            if (Physics.Raycast(ray, out hit, Mathf.Infinity, wallMask))
-            {
-                launchDirection = (hit.point - transform.localPosition).normalized;
-            }
+            launchDirection = (hit.point - GameManager.instance.PlatformCamera.transform.position).normalized;
+            launchPosition = GameManager.instance.PlatformCamera.transform.position;
+            hitMark.transform.position = hit.point;
+            hitMark.transform.up = hit.normal;
         }
     }
 }
